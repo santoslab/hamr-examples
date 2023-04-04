c0c35fd19f15eda584ae1ac8c9c1fff20b78cf81
Jason Belt <belt@ksu.edu>
2023-04-04 16:15:40 -0500

    add clean script

---
+++ isolette/aadl/bin/clean.cmd
@@ -0,0 +1,48 @@
+::#! 2> /dev/null                                   #
+@ 2>/dev/null # 2>nul & echo off & goto BOF         #
+if [ -z ${SIREUM_HOME} ]; then                      #
+  echo "Please set SIREUM_HOME env var"             #
+  exit -1                                           #
+fi                                                  #
+exec ${SIREUM_HOME}/bin/sireum slang run "$0" "$@"  #
+:BOF
+setlocal
+if not defined SIREUM_HOME (
+  echo Please set SIREUM_HOME env var
+  exit /B -1
+)
+%SIREUM_HOME%\\bin\\sireum.bat slang run "%0" %*
+exit /B %errorlevel%
+::!#
+// #Sireum
+
+import org.sireum._
+
+val hamrDir: Os.Path = Os.slashDir.up.up / "hamr"
+val slangDir = hamrDir / "slang"
+
+val toKeep = ops.ISZOps(ISZ(
+  (slangDir / "src" / "main" / "component"),
+  (slangDir / "src" / "test" / "bridge"),
+))
+
+
+def rec(p: Os.Path): Unit = {
+  if(p.isFile && !toKeep.contains(p)) {
+    println(s"Removing file ${p.value}")
+    p.remove()
+  } else {
+    if (toKeep.contains(p)) {
+      return
+    } else {
+      for(pp <- p.list) {
+        rec(pp)
+      }
+      if(p.list.isEmpty) {
+        println(s"Removing directory ${p.value}")
+        p.removeAll()
+      }
+    }
+  }
+}
+rec(hamrDir)
